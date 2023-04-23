202f91ff78f466274606646c0b0f0951a97f7d97
Debabrata Acharya <11374405+onclave@users.noreply.github.com>
2023-04-24 01:46:04 +0530

    add NVerseSecurityFilterChain

---
+++ src/main/java/com/bloomscorp/nverse/NVerseSecurityFilterChain.java
@@ -0,0 +1,63 @@
+package com.bloomscorp.nverse;
+
+import com.bloomscorp.alfred.LogBook;
+import com.bloomscorp.alfred.orm.AuthenticationLog;
+import com.bloomscorp.alfred.orm.Log;
+import com.bloomscorp.nverse.pojo.NVerseTenant;
+import org.jetbrains.annotations.NotNull;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.access.intercept.AuthorizationFilter;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+public class NVerseSecurityFilterChain<
+	B extends LogBook<L, A, T, E>,
+	L extends Log,
+	A extends AuthenticationLog,
+	T extends NVerseTenant<E>,
+	E extends Enum<E>
+> {
+
+	public SecurityFilterChain filterChain(
+		@NotNull HttpSecurity httpSecurity,
+		String[] nonAuthenticatedURLs,
+		String postAntMatcher,
+		NVerseAuthenticationEntryPoint authenticationEntryPoint,
+		NVerseRequestFilter<T, E> requestFilter,
+		NVerseExceptionHandlerFilter<B, L, A, T, E> exceptionHandlerFilter,
+		NVerseHttpRequestFilter httpRequestFilter
+	) throws Exception {
+
+		httpSecurity
+			.requiresChannel()
+			.anyRequest()
+			.requiresSecure()
+			.and()
+			.csrf()
+			.disable()
+			.cors()
+			.and()
+			.authorizeHttpRequests()
+			.requestMatchers(nonAuthenticatedURLs)
+			.permitAll()
+			.anyRequest()
+			.authenticated()
+			.and()
+			.exceptionHandling()
+			.authenticationEntryPoint(authenticationEntryPoint)
+			.and()
+			.sessionManagement()
+			.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+			.and()
+			.addFilterBefore(requestFilter, UsernamePasswordAuthenticationFilter.class)
+			.addFilterBefore(exceptionHandlerFilter, NVerseRequestFilter.class)
+			.securityMatcher(postAntMatcher)
+			.addFilterAfter(httpRequestFilter, AuthorizationFilter.class)
+			.headers()
+			.frameOptions()
+			.disable();
+
+		return httpSecurity.build();
+	}
+}
