e95f7045ecd73ff324f9f8a0470866b9ad5a8e21
Debabrata Acharya <11374405+onclave@users.noreply.github.com>
2023-04-23 22:34:17 +0530

    add NVerseCORSConfigurationSource

---
+++ src/main/java/com/bloomscorp/nverse/NVerseCORSConfigurationSource.java
@@ -0,0 +1,57 @@
+package com.bloomscorp.nverse;
+
+import com.bloomscorp.nverse.support.Constant;
+import org.springframework.http.HttpHeaders;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+public class NVerseCORSConfigurationSource {
+
+	public CorsConfigurationSource source(String uiOrigin) {
+
+		CorsConfiguration configuration = new CorsConfiguration();
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+
+		configuration.setAllowedOrigins(Collections.singletonList(uiOrigin));
+		configuration.setAllowCredentials(true);
+
+		configuration.setAllowedMethods(Arrays.asList(
+			Constant.METHOD_GET,
+			Constant.METHOD_PUT,
+			Constant.METHOD_POST,
+			Constant.METHOD_DELETE,
+			Constant.METHOD_OPTIONS,
+			Constant.METHOD_HEAD
+		));
+
+		configuration.setAllowedHeaders(Arrays.asList(
+			Constant.HEADER_X_REQUESTED_WITH,
+			HttpHeaders.ORIGIN,
+			HttpHeaders.CONTENT_TYPE,
+			HttpHeaders.ACCEPT,
+			HttpHeaders.AUTHORIZATION,
+			HttpHeaders.ACCESS_CONTROL_ALLOW_CREDENTIALS,
+			HttpHeaders.ACCESS_CONTROL_ALLOW_HEADERS,
+			HttpHeaders.ACCESS_CONTROL_ALLOW_METHODS,
+			HttpHeaders.ACCESS_CONTROL_ALLOW_ORIGIN,
+			HttpHeaders.ACCESS_CONTROL_EXPOSE_HEADERS,
+			HttpHeaders.ACCESS_CONTROL_MAX_AGE,
+			HttpHeaders.ACCESS_CONTROL_REQUEST_HEADERS,
+			HttpHeaders.ACCESS_CONTROL_REQUEST_METHOD,
+			HttpHeaders.AGE,
+			HttpHeaders.ALLOW,
+			Constant.HEADER_ALTERNATES,
+			HttpHeaders.CONTENT_RANGE,
+			HttpHeaders.CONTENT_DISPOSITION,
+			Constant.HEADER_DESCRIPTION
+		));
+
+		source.registerCorsConfiguration("/**", configuration);
+
+		return source;
+	}
+}
--- src/main/java/com/bloomscorp/nverse/support/Constant.java
+++ src/main/java/com/bloomscorp/nverse/support/Constant.java
@@ -10,4 +10,19 @@ public final class Constant {
     public static final String END_OF_STRING_VALUE = "\0";
     public static final String REQUEST_HEADER_AUTHORIZATION = "Authorization";
     public static final String REQUEST_HEADER_VALUE_BEARER = "Bearer ";
+
+    /*--CORS METHOD CONSTANTS--*/
+
+    public static final String METHOD_GET = "GET";
+    public static final String METHOD_PUT = "PUT";
+    public static final String METHOD_POST = "POST";
+    public static final String METHOD_DELETE = "DELETE";
+    public static final String METHOD_OPTIONS = "OPTIONS";
+    public static final String METHOD_HEAD = "HEAD";
+
+    /*--CORS HEADERS CONSTANTS--*/
+
+    public static final String HEADER_X_REQUESTED_WITH = "X-Requested-With";
+    public static final String HEADER_ALTERNATES = "Alternates";
+    public static final String HEADER_DESCRIPTION = "Content-Description";
 }
